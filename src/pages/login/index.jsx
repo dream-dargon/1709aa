14fda54da5b87b31e161c64caa5fc88148928065
dream-dargon <330798125@qq.com>
2020-05-17 16:34:09 +0800

    '创建请求拦截器'

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,10 +1,52 @@
 import React, { Component } from 'react'
+import { Form, Input, Button, Icon } from 'antd';
+import { Link } from 'react-router-dom'
+import './style.less'
 
-export default class Login extends Component {
+export default @Form.create({})
+class Login extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
-      <div>
-        登录
+      <div className="pages-login">
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <Form.Item>
+            {getFieldDecorator('username', {
+              rules: [{ required: true, message: '请输入姓名!' }],
+            })(
+              <Input
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="Username"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('pwd', {
+              rules: [{ required: true, message: '请输入密码!' }],
+            })(
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="password"
+                placeholder="Password"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log in
+            </Button>
+            Or <Link to="/reg">register now!</Link>
+          </Form.Item>
+        </Form>
       </div>
     )
   }
