c54969e5153cabef96f99964aca9f50e19135e72
dream-dargon <330798125@qq.com>
2020-05-17 16:14:09 +0800

    '创建登录注册文件'

--- src/router/assembly.js
+++ src/router/assembly.js
@@ -1,6 +1,9 @@
 import Loadable from '@@/Loadable'
 const App = Loadable(() => import('@/pages/app'))
+const Login = Loadable(() => import('@/pages/login'))
+const Reg = Loadable(() => import('@/pages/reg'))
 export {
   App,
-  // Login,
+  Login,
+  Reg,
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { App } from '@/router/assembly'
+import { App, Login, Reg } from '@/router/assembly'
 
 export default class Router extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/reg" component={Reg} />
           <Route path="/" component={App} />
         </Switch>
       </BrowserRouter>
